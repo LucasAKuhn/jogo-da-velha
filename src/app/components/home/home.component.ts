d39c868478f4813cc028153b81dd823977662466
lucas <lucasa.kuhn@hotmail.com>
2024-07-07 14:13:48 -0300

    Criando home e header

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  squares:any = [];
+  winner = '';
+  counter = 0;
+  isdraw = '';
+  freshpage = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  newGame(){
+    this.squares = Array(9).fill(null);
+    this.winner = '';
+    this.isdraw = '';
+    this.counter = 0;
+    this.freshpage = false;
+  }
 
 }
