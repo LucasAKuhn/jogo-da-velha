9e535cb55a50cc8e8a39eecb25dbf85b33504200
lucas <lucasa.kuhn@hotmail.com>
2024-07-08 11:23:58 -0300

    Corrigido logica para vitoria

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,25 +1,13 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements AfterViewInit {
-  ngAfterViewInit() {
-    document.querySelectorAll('.nav-item').forEach(anchor => {
-      anchor.addEventListener('click', (e) => {
-        e.preventDefault();
+export class HeaderComponent {
 
-        const targetId = anchor.getAttribute('href')?.substring(1); // Use optional chaining to handle potential null
-        const targetElement = document.getElementById(targetId!); // Use non-null assertion operator
-
-        if (targetElement) {
-          targetElement.scrollIntoView({
-            behavior: 'smooth'
-          });
-        }
-      });
-    });
+  teste() {
+    console.log('teste');
   }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -49,7 +49,7 @@ export class HomeComponent implements OnInit {
   calculateWinner(){
     const lines = [
       [0,1,2],[3,4,5],[6,7,8],
-      [0,2,6],[1,4,7],[2,5,8],
+      [0,3,6],[1,4,7],[2,5,8],
       [0,4,8],[2,4,6]
     ]
 
