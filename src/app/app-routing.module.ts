8bb51c427ef939d79004a8bc1ea3d3a3523eff89
lucas <lucasa.kuhn@hotmail.com>
2024-07-08 09:32:53 -0300

    Alterado layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
+import {BoardComponent} from "./components/board/board.component";
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'board',
+    component: BoardComponent
   }
 ];
 
