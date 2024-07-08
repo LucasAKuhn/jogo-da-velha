59319e428dadf5513d232339543318006cf43590
lucas <lucasa.kuhn@hotmail.com>
2024-07-08 09:16:49 -0300

    Criando estrutura do jogo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent}  from "./components/header/header.component";
 import { HomeComponent } from './components/home/home.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
